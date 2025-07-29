72c8e8b4fd631b5b3a3b77a94b6a2e9c317d0830
cowen20 <cowen20@uic.edu>
2025-07-29 17:36:31 -0500

    Calculate number/portion of reads/bases removed by fastp

--- read_fastp_reports.R
+++ read_fastp_reports.R
@@ -20,8 +20,8 @@ unt_fastp_no_dedup_dir <- paste0(topdir,
 read_fastp_report <- function(fastp_json_path) {
   fastp_json <- fromJSON(fastp_json_path)
   fastp_df <- as.data.frame(fastp_json[-c(4:9)])
-  fastp_df$insert_size_peak <- fastp_json[[4]][1]
-  fastp_df$unknown_ins_size_count <- fastp_json[[4]][2]
+  fastp_df$insert_size_peak <- fastp_json[[4]][[1]]
+  fastp_df$unknown_ins_size_count <- fastp_json[[4]][[2]]
   fastp_df$SampleID <- basename(fastp_json_path)
   return(fastp_df)
 }
@@ -41,6 +41,16 @@ import_fastp_summaries <- function(directory) {
                             remove = FALSE)
   #parse_sample_treatments is defined in import_k2_results.R
   fastp_reports <- parse_sample_treatments(fastp_reports)
+  fastp_reports <- mutate(fastp_reports,
+                    num_reads_removed = summary.before_filtering.total_reads - 
+                      summary.after_filtering.total_reads,
+                    portion_reads_removed = num_reads_removed / 
+                      summary.before_filtering.total_reads,
+                    num_bases_removed = summary.before_filtering.total_bases -
+                      summary.after_filtering.total_bases,
+                    portion_bases_removed = num_bases_removed /
+                      summary.before_filtering.total_bases
+                    )
   return(fastp_reports)
 }
 
@@ -59,13 +69,6 @@ unt_fastp_summaries_unmerged <- import_fastp_summaries(
   unt_fastp_no_dedup_dir
   ) %>%
   mutate(Enrichment = "None")
-
-#Calculate portion of read pairs removed by fastp:
-rpip_fastp_summaries <- rpip_fastp_summaries %>%
-  mutate(portion_pairs_removed = 1 - (after_filtering.total_reads / (before_filtering.total_reads/2)))
-
-untargeted_fastp_summaries <- untargeted_fastp_summaries %>%
-  mutate(portion_pairs_removed = 1 - (after_filtering.total_reads / (before_filtering.total_reads/2)))
 
 #combine them
 all_fastp_summaries_unmerged <- rbind(rpip_fastp_summaries_unmerged,
