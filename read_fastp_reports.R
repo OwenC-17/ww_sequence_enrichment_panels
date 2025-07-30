f2d720cbdd85cac1d66f3e82d48dbc845e4d0b0c
cowen20 <cowen20@uic.edu>
2025-07-30 15:04:05 -0500

    Clean sample id parsing for potential different filename lenths

--- read_fastp_reports.R
+++ read_fastp_reports.R
@@ -74,3 +74,79 @@ unt_fastp_summaries_unmerged <- import_fastp_summaries(
 all_fastp_summaries_unmerged <- rbind(rpip_fastp_summaries_unmerged,
                                       vsp_fastp_summaries_unmerged,
                                       unt_fastp_summaries_unmerged)
+
+ggplot(all_fastp_summaries_unmerged, aes(x = Enrichment, y = portion_reads_removed)) + geom_boxplot()
+ggplot(all_fastp_summaries_unmerged, aes(x = Enrichment, y = portion_bases_removed)) + geom_boxplot()
+ggplot(all_fastp_summaries_unmerged, aes(x = Enrichment, y = summary.after_filtering.total_reads)) + geom_boxplot()
+
+
+################################
+###Import merged read reports###
+################################
+#Function to load one fastp report:
+read_merged_fastp_report <- function(fastp_json_path) {
+  fastp_json <- fromJSON(fastp_json_path)
+  fastp_df <- as.data.frame(fastp_json[-c(4:8)])
+  fastp_df$insert_size_peak <- fastp_json[[4]][[1]]
+  fastp_df$unknown_ins_size_count <- fastp_json[[4]][[2]]
+  fastp_df$SampleID <- basename(fastp_json_path)
+  return(fastp_df)
+}
+
+#Function to load ALL fastp reports:
+import_merged_fastp_summaries <- function(directory) {
+  file_list <- list.files(directory, pattern = "*\\.json$", full.names = TRUE)
+  fastp_reports <- lapply(file_list, read_merged_fastp_report)
+  fastp_reports <- bind_rows(fastp_reports)
+  fastp_reports <- separate(fastp_reports,
+                            col = SampleID,
+                            into = c("QCSeqID", "LIMS_ID", "Treatment"),
+                            sep = "(-|_)",
+                            remove = FALSE,
+                            extra = "drop")
+  #parse_sample_treatments is defined in import_k2_results.R
+  fastp_reports <- parse_sample_treatments(fastp_reports)
+  fastp_reports <- mutate(fastp_reports,
+                          num_reads_removed = summary.before_filtering.total_reads - 
+                            (2 * summary.after_filtering.total_reads),
+                          portion_reads_removed = num_reads_removed / 
+                            summary.before_filtering.total_reads,
+                          num_bases_removed = summary.before_filtering.total_bases -
+                            summary.after_filtering.total_bases,
+                          portion_bases_removed = num_bases_removed /
+                            summary.before_filtering.total_bases
+  )
+  return(fastp_reports)
+}
+
+
+rpip_fastp_merged_no_dedup_dir <- paste0(topdir,
+                                         "rpip_panels/raw_fastqs/",
+                                         "fastp_merged_no_dedup/reports/")
+
+unt_fastp_merged_no_dedup_dir <- paste0(topdir,
+                                         "untargeted/raw_fastqs/",
+                                         "fastp_merged_no_dedup/reports/")
+
+test_ <- "input/link_to_raw_data/rpip_panels/raw_fastqs/fastp_merged_no_dedup/reports/001-36397-FA-RP_S265_L005_merged_no_dedup_fastp_data.json"
+
+merged_ <- fromJSON(test_)
+
+rpip_fastp_summaries_merged <- import_merged_fastp_summaries(
+  rpip_fastp_merged_no_dedup_dir
+  ) %>%
+  mutate(Enrichment = "RPIP")
+
+unt_fastp_summaries_merged <- import_merged_fastp_summaries(
+  unt_fastp_merged_no_dedup_dir
+  ) %>%
+  mutate(Enrichment = "None")
+
+#Combine
+both_fastp_summaries_merged <- rbind(rpip_fastp_summaries_merged,
+                                     unt_fastp_summaries_merged)
+
+ggplot(both_fastp_summaries_merged, aes(x = Enrichment, y = portion_reads_removed)) + geom_boxplot()
+ggplot(both_fastp_summaries_merged, aes(x = Enrichment, y = portion_bases_removed)) + geom_boxplot()
+ggplot(both_fastp_summaries_merged, aes(x = Enrichment, y = summary.after_filtering.total_reads)) + geom_boxplot()
+ggplot(both_fastp_summaries_merged, aes(x = Enrichment, y = summary.before_filtering.total_reads)) + geom_boxplot()
