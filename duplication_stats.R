eeaa260e9e1f51a8a36622719e72d5fecae52247
cowen20 <cowen20@uic.edu>
2025-08-15 15:53:57 -0500

    Incorporate dup rates from deduplication reports

--- duplication_stats.R
+++ duplication_stats.R
@@ -7,6 +7,8 @@ all_fastp_summaries_unmerged$PreEnrichmentID <- paste(
 
 library(lme4)
 
+
+####Based on pre-deduplication reports:
 lmer_dupRate_Enrichment_model <- lmer(rate ~ Enrichment + 
                                   (1|PreEnrichmentID),
                                 data = all_fastp_summaries_unmerged)
@@ -57,6 +59,31 @@ wilcox_dupRate_Enrichment <- pairwise.wilcox.test(
 wilcox_dupRate_Enrichment$p.value
 
 
+##Based on post-duplication reports:
+all_dedup_reports$PreEnrichmentID <- paste(
+  all_dedup_reports$LIMS_ID,
+  all_dedup_reports$Fraction,
+  all_dedup_reports$Nanotrap_type,
+  sep = "_"
+)
+
+fr_dupRate_Enrichment_model_post <- friedman.test(rate ~ Enrichment | PreEnrichmentID,
+                                                  data = all_dedup_reports)
+fr_dupRate_Enrichment_model_post$p.value
+
+#Post-hoc Wilcoxon Signed-Rank Tests
+all_dedup_reports <- all_dedup_reports %>%
+  group_by(Enrichment) %>%
+  arrange(PreEnrichmentID)
+
+wilcox_dupRate_Enrichment_post <- pairwise.wilcox.test(
+  all_dedup_reports$rate, 
+  all_dedup_reports$Enrichment,
+  paired = TRUE,
+  p.adjust.method = "none")
+
+wilcox_dupRate_Enrichment_post$p.value
+
 ####Plot dup comparison:
 library(ggpubr)
 library(tidyverse)
@@ -66,6 +93,23 @@ custom_label <- function(p) {
 }
 
 ggplot(all_fastp_summaries_unmerged, aes(x = Enrichment, y = rate)) +
+  geom_boxplot() + 
+  theme_minimal() +
+  stat_compare_means(method = "wilcox.test",
+                     comparisons = list(c("RPIP", "None"),
+                                        c("VSP", "RPIP"),
+                                        c("VSP", "None")),
+                     paired = TRUE,
+                     label = "p.format",
+                     digits = 10,
+                     size = 5) +
+  ylab("Read duplication rate") +
+  xlab("Enrichment panel") +
+  theme(axis.text = element_text(size = 15), 
+        axis.title = element_text(size = 20))
+
+
+ggplot(all_dedup_reports, aes(x = Enrichment, y = rate)) +
   geom_boxplot() + 
   theme_minimal() +
   stat_compare_means(method = "wilcox.test",
