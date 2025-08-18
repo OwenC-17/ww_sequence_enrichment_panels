4f2fe10a210938eac54432f0f1d8b676f00a589b
cowen20 <cowen20@uic.edu>
2025-08-18 14:12:24 -0500

    Export deduplicated family count tables for EdgeR

---
+++ edger_prep_dedup.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(data.table)
+library(edgeR)
+
+source("helper_functions.R")
+#Main dir that contains all targeted panel results
+saved_reports_path <- "imported_k2_reports/all_k2_reports_anyConf.csv"
+saved_dedup_reports_path <- paste0("imported_k2_reports/",
+                                   "all_k2_reports_anyConf_dedup.csv")
+
+#Function to apply after importing (there are too many columns to specify types
+#during import)
+assert_k2_report_coltypes <- function(k2_report_df) {
+  fixed <- k2_report_df %>%
+    mutate(taxID = as_factor(as.character(taxID)),
+           nodeAndChildren = as.integer(nodeAndChildren),
+           nodeOnly = as.integer(nodeOnly),
+           LIMS_ID = as_factor(LIMS_ID),
+           Treatment = as_factor(Treatment),
+           ribosomal = as_factor(ribosomal),
+           site = as_factor(site),
+           Enrichment = as_factor(Enrichment),
+           #sprintf is used to preserve correct digit length in type conversion
+           Kraken2_confidence = as_factor(sprintf("%.1f", Kraken2_confidence)))
+  return(fixed)
+}
+
+#######################
+#####Load the data#####
+#######################
+all_k2_reports_anyConf <- read_csv(saved_reports_path, guess_max = Inf)
+all_k2_reports_anyConf <- assert_k2_report_coltypes(all_k2_reports_anyConf)
+all_k2_reports_anyConf <- parse_sample_treatments(all_k2_reports_anyConf)
+###Deduped versions###
+all_k2_reports_deduped <- read_csv(saved_dedup_reports_path, guess_max = Inf)
+all_k2_reports_deduped <- assert_k2_report_coltypes(all_k2_reports_deduped)
+all_k2_reports_deduped <- parse_sample_treatments(all_k2_reports_deduped)
+######################
+
+#To summarize the imported reports at any particular tax level
+collapse_to_tax_level <- function(imported_k2_reports, level) {
+  collapsed <- imported_k2_reports
+  setDT(collapsed)
+  collapsed[, .(readcount = sum(nodeOnly), RA = sum(RA)),
+            by = c(level, "LIMS_ID", "site", "Treatment", 
+                   "Enrichment", "ribosomal", "Kraken2_confidence", "Fraction",
+                   "Nanotrap_type", "dedup")]
+}
+
+collapse_to_family_no_rrna <- function(imported_k2_report) {
+  families <- collapse_to_tax_level(imported_k2_report, "F")
+  families <- filter(families, ribosomal == "nonrrna")
+}
+
+allFamilies_anyConf_noRrna <- collapse_to_family_no_rrna(all_k2_reports_anyConf)
+allFamilies_anyConf_noRrna_dedup <- collapse_to_family_no_rrna(all_k2_reports_deduped)
+
+collapse_to_family_keep_rrna <- function(imported_k2_report) {
+  families <- collapse_to_tax_level(imported_k2_report, "F")
+  families <- families %>%
+    group_by(across(-c(readcount, RA, ribosomal))) %>%
+    summarise(readcount = sum(readcount), .groups = "drop") %>%
+    group_by(across(-c(`F`, readcount))) %>%
+    mutate(RA = readcount / sum(readcount))
+}
+
+allFamilies_anyConf_withRrna <- collapse_to_family_keep_rrna(
+  all_k2_reports_anyConf
+  )
+
+allFamilies_anyConf_withRrna_dedup <- collapse_to_family_keep_rrna(
+  all_k2_reports_deduped
+)
+
+################################################################################
+allFamilies_anyConf_noRrna <- allFamilies_anyConf_noRrna %>%
+  mutate(UniqueID = paste(LIMS_ID, Treatment, Enrichment, Kraken2_confidence, sep = "-"))
+
+allFamilies_anyConf_noRrna_dedup <- allFamilies_anyConf_noRrna_dedup %>%
+  mutate(UniqueID = paste(LIMS_ID, Treatment, Enrichment, Kraken2_confidence, sep = "-"))
+
+allFamilies_anyConf_withRrna <- allFamilies_anyConf_withRrna %>%
+  mutate(UniqueID = paste(LIMS_ID, Treatment, Enrichment, Kraken2_confidence, sep = "-"))
+
+
+allFamilies_anyConf_withRrna_dedup <- allFamilies_anyConf_withRrna_dedup %>%
+  mutate(UniqueID = paste(LIMS_ID, Treatment, Enrichment, Kraken2_confidence, sep = "-"))
+
+
+sample_metadata_noRrna <- allFamilies_anyConf_noRrna %>%
+  ungroup() %>%
+  select(UniqueID, LIMS_ID, Treatment, site, Fraction, Nanotrap_type,
+         Enrichment, Kraken2_confidence) %>%
+  distinct()
+
+
+sample_metadata_withRrna <- allFamilies_anyConf_withRrna %>%
+  ungroup() %>%
+  select(UniqueID, LIMS_ID, Treatment, site, Fraction, Nanotrap_type,
+         Enrichment, Kraken2_confidence) %>%
+  distinct()
+
+family_count_matrix_noRrna <- allFamilies_anyConf_noRrna %>%
+  pivot_wider(id_cols = !!sym("F"),
+              names_from = UniqueID,
+              values_from = readcount,
+              values_fill = 0)
+
+family_count_matrix_noRrna_dedup <- allFamilies_anyConf_noRrna_dedup %>%
+  pivot_wider(id_cols = !!sym("F"),
+              names_from = UniqueID,
+              values_from = readcount,
+              values_fill = 0)
+
+
+family_count_matrix_withRrna_dedup <- allFamilies_anyConf_withRrna_dedup %>%
+  pivot_wider(id_cols = !!sym("F"),
+              names_from = UniqueID,
+              values_from = readcount,
+              values_fill = 0)
+
+
+write_csv(sample_metadata_noRrna, 
+          "edger_tables/edger_sample_metadata_noRrna.csv")
+write_csv(sample_metadata_withRrna, 
+          "edger_tables/edger_sample_metadata_withRrna.csv")
+
+write_csv(family_count_matrix_noRrna, 
+          "edger_tables/edger_family_count_matrix_noRrna.csv")
+write_csv(family_count_matrix_noRrna_dedup, 
+          "edger_tables/edger_family_count_matrix_noRrna_dedup.csv")
+
+write_csv(family_count_matrix_withRrna_dedup, 
+          "edger_tables/edger_family_count_matrix_withRrna_dedup.csv")
