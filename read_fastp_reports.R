012ddfe30b9c6542e8f977c9b490e2b53112542a
cowen20 <cowen20@uic.edu>
2026-05-15 17:57:44 -0500

    Clarify "filename" column; combine QC and dedup reports

--- read_fastp_reports.R
+++ read_fastp_reports.R
@@ -2,6 +2,7 @@ library(jsonlite)
 library(tidyverse)
 
 source("helper_functions.R")
+
 ###Define filepaths
 topdir <- "input/link_to_raw_data/"
 vsp_fastp_no_dedup_dir <- paste0(topdir,
@@ -22,7 +23,7 @@ read_fastp_report <- function(fastp_json_path) {
   fastp_df <- as.data.frame(fastp_json[-c(4:9)])
   fastp_df$insert_size_peak <- fastp_json[[4]][[1]]
   fastp_df$unknown_ins_size_count <- fastp_json[[4]][[2]]
-  fastp_df$SampleID <- basename(fastp_json_path)
+  fastp_df$filename <- basename(fastp_json_path)
   return(fastp_df)
 }
 
@@ -34,7 +35,7 @@ import_fastp_summaries <- function(directory) {
   fastp_reports <- lapply(file_list, read_fastp_report)
   fastp_reports <- bind_rows(fastp_reports)
   fastp_reports <- separate(fastp_reports,
-                            col = SampleID,
+                            col = filename,
                             into = c("QCSeqID", "LIMS_ID", "Treatment",
                                      NA, NA, NA, NA, NA),
                             sep = "(-|_)",
@@ -71,12 +72,13 @@ unt_fastp_summaries_unmerged <- import_fastp_summaries(
   ) %>%
   mutate(Enrichment = "None")
 
-#combine them
+#Combine them:
 all_fastp_summaries_unmerged <- rbind(rpip_fastp_summaries_unmerged,
                                       vsp_fastp_summaries_unmerged,
                                       unt_fastp_summaries_unmerged)
 
 
+#Write files for later use:
 write_rds(all_fastp_summaries_unmerged,
           "input/modified/fastp_summaries_unmerged_before_dedup.rds")
 
@@ -105,10 +107,48 @@ all_dedup_reports <- rbind(vsp_dedup_report,
                            rpip_dedup_report,
                            unt_dedup_report)
 
+
 write_rds(all_dedup_reports, "input/modified/fastp_dedup_reports.rds")
 
 write_csv(all_dedup_reports, "input/modified/fastp_dedup_reports.csv")
 
+################################
+#####Join dup/dedup reports#####
+################################
+
+#Pick columns that are not redundant:
+dedup_relevant <- all_dedup_reports %>%
+  select(summary.after_filtering.gc_content:summary.after_filtering.total_reads,
+         insert_size_peak:Enrichment)
+
+#Rename columns to reflect that they represent post-deduplication stats:
+colnames(dedup_relevant) <- str_replace_all(colnames(dedup_relevant), 
+                                        c("filtering" = "dedup", 
+                                          "removed" = "removed_dedup",
+                                          "insert_size_peak" = "insert_size_peak_dedup",
+                                          "filename" = "filename_dedup",
+                                          "unknown_ins_size_count" = "unknown_ins_size_count_dedup"))
+
+#Join to pre-deduplicated data:
+all_fastp_reports_dupdedup <- all_fastp_summaries_unmerged %>%
+  left_join(dedup_relevant)
+
+all_fastp_reports_dupdedup <- all_fastp_reports_dupdedup %>%
+  mutate(num_reads_removed_qcANDdedup = summary.before_filtering.total_reads -
+           summary.after_dedup.total_reads,
+         portion_reads_removed_qcANDdedup = num_reads_removed_qcANDdedup /
+           summary.before_filtering.total_reads,
+         num_bases_removed_qcANDdedup = summary.before_filtering.total_bases -
+           summary.after_dedup.total_bases,
+         portion_bases_removed_qcANDdedup = num_bases_removed_qcANDdedup /
+           summary.before_filtering.total_bases)
+
+#Write files for later:
+write_rds(all_fastp_reports_dupdedup, 
+          "input/modified/all_fastp_reports_dupdedup.rds")
+
+write_csv(all_fastp_reports_dupdedup,
+          "input/modified/all_fastp_reports_dupdedup.csv")
 
 ####################################
 #####Import merged read reports#####
@@ -119,7 +159,7 @@ read_merged_fastp_report <- function(fastp_json_path) {
   fastp_df <- as.data.frame(fastp_json[-c(4:8)])
   fastp_df$insert_size_peak <- fastp_json[[4]][[1]]
   fastp_df$unknown_ins_size_count <- fastp_json[[4]][[2]]
-  fastp_df$SampleID <- basename(fastp_json_path)
+  fastp_df$filename <- basename(fastp_json_path)
   return(fastp_df)
 }
 
@@ -129,7 +169,7 @@ import_merged_fastp_summaries <- function(directory) {
   fastp_reports <- lapply(file_list, read_merged_fastp_report)
   fastp_reports <- bind_rows(fastp_reports)
   fastp_reports <- separate(fastp_reports,
-                            col = SampleID,
+                            col = filename,
                             into = c("QCSeqID", "LIMS_ID", "Treatment"),
                             sep = "(-|_)",
                             remove = FALSE,
@@ -152,20 +192,24 @@ import_merged_fastp_summaries <- function(directory) {
 }
 
 
+#Specify report locations:
 rpip_fastp_merged_no_dedup_dir <- paste0(topdir,
                                          "rpip_panels/raw_fastqs/",
                                          "fastp_merged_no_dedup/reports/")
+unt_fastp_merged_no_dedup_dir <- paste0(topdir,
+                                        "untargeted/raw_fastqs/",
+                                        "fastp_merged_no_dedup/reports/")
+
 rpip_fastp_merged_then_dedup_dir <- paste0(
   topdir, "rpip_panels/raw_fastqs/fastp_merged_no_dedup/deduped"
 )
 
-unt_fastp_merged_no_dedup_dir <- paste0(topdir,
-                                         "untargeted/raw_fastqs/",
-                                         "fastp_merged_no_dedup/reports/")
 unt_fastp_merged_then_dedup_dir <- paste0(
   topdir, "untargeted/raw_fastqs/fastp_merged_no_dedup/deduped"
 )
 
+
+#Import the reports:
 rpip_fastp_summaries_merged <- import_merged_fastp_summaries(
   rpip_fastp_merged_no_dedup_dir
   ) %>%
@@ -193,6 +237,7 @@ both_fastp_summaries_merged <- rbind(rpip_fastp_summaries_merged,
 both_fastp_summaries_merged_dedup <- rbind(rpip_fastp_summaries_merged_dedup,
                                            unt_fastp_summaries_merged_dedup)
 
+#Write files for later use:
 write_rds(both_fastp_summaries_merged, 
           "input/modified/fastp_summaries_merged_before_dedup.rds")
 
